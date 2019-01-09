412fd3b81e1947a06d44b7f100ec812af3b1d348
arshuShaik <46276979+arshuShaik@users.noreply.github.com>
2019-01-09 16:50:46 +0530

    change2
    change2

--- local-git-jenkins/src/demo/first.java
+++ local-git-jenkins/src/demo/first.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class first {
 @Test
 public void demogitjen() {
-	System.out.println("running test from github");
+	System.out.println("running test from gii");
 	
 	
 }
